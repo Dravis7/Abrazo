97e6903b9712fced474e8042b1c174c04f4d1444
Dravis7 <82489025+Dravis7@users.noreply.github.com>
2023-06-28 21:24:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+setTimeout(cambiamensaje,2300)
+
+function cambiamensaje() {
+    document.getElementById("loading-mesage").innerText = "Enviado"
+}
+
+setTimeout(function () {
+    alert("Recibiste un Abrazo :3")
+}, 2400)
